9279ce149a1a3652deeb76ea78bee36ef9b4a21b
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-01-04 19:20:05 +0530

    [NIJAM] Add:Retrive ProductId and Review Using Select operator

--- ProductReviewManagement_Linq_CSharp/ProductReviewManagement.cs
+++ ProductReviewManagement_Linq_CSharp/ProductReviewManagement.cs
@@ -48,7 +48,7 @@ namespace ProductReviewManagement_Linq_CSharp
         public void RetrieveIdAndReview(List<ProductReview> listProductReview)
         {
             var recordedData = listProductReview.Select(x => new { ProductID = x.ProductId, Review = x.Review });
-            Console.WriteLine("ID       Review");
+            Console.WriteLine("ID------>Review");
             foreach (var list in recordedData)
             {
                 Console.WriteLine(list.ProductID + "------>" + list.Review);
